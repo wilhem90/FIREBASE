d847680e6e86b24b5a172ac0d3668708fd354620
Wilhem Wundt Maxime <wilhemmaxime97@gmail.com>
2023-01-07 16:46:22 -0300

    refresh

--- CASHMAX/script.js
+++ CASHMAX/script.js
@@ -4,3 +4,9 @@ const addModal = document.querySelector('.add-modal');
 btnAdd.addEventListener('click', () => {
     addModal.classList.add('modal-show')
 })
+
+// Uuario click qualquer lugar fora do modal para fechar o mesmo
+
+window.addEventListener('click', e => {
+    console.log(e.target, addModal);
+})
