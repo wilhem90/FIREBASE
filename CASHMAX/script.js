af84955b23cef80ef86d0d3040238c6b27f1b938
Wilhem Wundt Maxime <wilhemmaxime97@gmail.com>
2023-01-07 05:11:41 -0300

    refresh

---
+++ CASHMAX/script.js
@@ -0,0 +1,6 @@
+const btnAdd = document.querySelector('.btn-add');
+const addModal = document.querySelector('.add-modal');
+
+btnAdd.addEventListener('click', () => {
+    addModal.classList.add('modal-show')
+})
