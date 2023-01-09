65ab68312b5f1705dcd77765bf9fde96ce36e3d5
Wilhem Wundt Maxime <wilhemmaxime97@gmail.com>
2023-01-09 02:37:04 -0300

    refresh

--- CASHMAX/script.js
+++ CASHMAX/script.js
@@ -8,5 +8,29 @@ btnAdd.addEventListener('click', () => {
 // Uuario click qualquer lugar fora do modal para fechar o mesmo
 
 window.addEventListener('click', e => {
-    console.log(e.target, addModal);
+    if (e.target === addModal) {
+        // alert('Fechamos o modal-show')
+        addModal.classList.remove('modal-show')
+    }
 })
+
+// Let get all user
+// let db = firebase.firestore();
+
+// db.collection('users').get().then(querySnapshot => {
+//     querySnapshot.forEach(doc => {
+//         console.log(doc.data());
+//     })
+// })
+
+
+// const addd = document.getElementById('addd')
+
+// addd.addEventListener('click', e => {
+//     addDoc(collection(db, "cities"), {
+//     name: "Los Angeles",
+//     state: "CA",
+//     country: "USA"
+//     });
+//         });
+
